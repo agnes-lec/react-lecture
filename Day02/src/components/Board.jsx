cfe8516b5bc8d93b856785177ae3e03da3c7200c
agnes-lec <whachozang@yonsei.ac.kr>
2025-12-04 22:49:36 +0900

    Refactor Board component to use JSX syntax
    Refactored Board component to use JSX for rendering squares instead of React.createElement. Updated the export statement to default export the Board component directly.

--- Day02/src/components/Board.jsx
+++ Day02/src/components/Board.jsx
@@ -1,21 +1,7 @@
 import React from 'react'
 import Square from './Square'
 
-function Board() {
-
-  const renderSquare = (i) => {
-    // React.createElement를 사용하여 Square 컴포넌트 생성
-    // 첫 번째 인자: 컴포넌트 타입 (Square)
-    // 두 번째 인자: props 객체 (value, onClick)
-    // 세 번째 인자: children (없음)
-    return React.createElement(
-      Square,                    // 컴포넌트 타입
-      {                          // props 객체
-        value: null,             // 현재 값: null (빈 칸)
-        onClick: () => {}        // 현재 클릭 핸들러: 빈 함수 (향후 게임 로직 추가 예정)
-      }
-    )
-  }
+export default function Board() {
 
   return (
     <div className="board">
@@ -23,13 +9,17 @@ function Board() {
         다음 플레이어: X
       </div>
       <div className="board-grid">
-        {Array.from({ length: 9 }).map((_, i) => (
-          <div key={i}>{renderSquare(i)}</div>
-        ))}
+        <div><Square value={1} onClick={() => {}}/></div>
+        <div><Square value={2} onClick={() => {}}/></div>
+        <div><Square value={3} onClick={() => {}}/></div>
+        <div><Square value={4} onClick={() => {}}/></div>
+        <div><Square value={5} onClick={() => {}}/></div>
+        <div><Square value={6} onClick={() => {}}/></div>
+        <div><Square value={7} onClick={() => {}}/></div>
+        <div><Square value={8} onClick={() => {}}/></div>
+        <div><Square value={9} onClick={() => {}}/></div>
       </div>
     </div>
   )
 }
-
-export default Board
 
