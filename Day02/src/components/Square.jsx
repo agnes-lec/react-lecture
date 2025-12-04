5cfe72cbcb36b11d6916673753c8df628fd3bb5b
agnes-lec <whachozang@yonsei.ac.kr>
2025-12-04 16:14:03 +0900

    컴포넌트 구현 실습 프로젝트

---
+++ Day02/src/components/Square.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Square({ value, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="square"
+    >
+      {value}
+    </button>
+  )
+}
+
+export default Square
+
